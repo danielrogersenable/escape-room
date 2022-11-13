20dee108ce682a892320b1fe9ef77ada26d88b30
Daniel Rogers <daniel.rogers@enable.com>
2022-11-13 18:08:16 +0000

    Puzzle 10

--- src/app/areas/puzzle10/puzzle10.component.ts
+++ src/app/areas/puzzle10/puzzle10.component.ts
@@ -26,7 +26,7 @@ export class Puzzle10Component implements OnInit {
 
   public displayError = false;
 
-  public correctAnswer = 'placeholder';
+  public correctAnswer = 'apple';
   public errorMessage = 'That wasn\'t right. Please try again.';
 
   public handleCorrectAnswerProvided(): void {
