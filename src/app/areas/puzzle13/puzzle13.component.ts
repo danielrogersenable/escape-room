45f39dbe23de26404629c4ff3ab5842b4975c5a5
Daniel Rogers <daniel.rogers@enable.com>
2022-11-15 21:12:23 +0000

    Puzzle 13

--- src/app/areas/puzzle13/puzzle13.component.ts
+++ src/app/areas/puzzle13/puzzle13.component.ts
@@ -11,6 +11,7 @@ export class Puzzle13Component implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this._buildLightArray();
   }
 
   @Input()
@@ -26,7 +27,7 @@ export class Puzzle13Component implements OnInit {
 
   public displayError = false;
 
-  public correctAnswer = 'placeholder';
+  public correctAnswer = 'blueprint';
   public errorMessage = 'That wasn\'t right. Please try again.';
 
   public handleCorrectAnswerProvided(): void {
@@ -36,5 +37,63 @@ export class Puzzle13Component implements OnInit {
 
   public handleClose(): void {
     this.close.emit();
+  }
+
+  public lightArray: boolean[][] = [];
+
+  public maxX = 4;
+  public maxY = 4;
+
+  private _buildLightArray() {
+    for (var x = 0; x <= this.maxX; x++) {
+      this.lightArray.push([]);
+      for (var y = 0; y <= this.maxY; y++) {
+        this.lightArray[x].push(false);
+      }
+    }
+
+    this.lightArray[0][1] = true;
+    this.lightArray[0][3] = true;
+    this.lightArray[1][1] = true;
+    this.lightArray[2][3] = true;
+    this.lightArray[3][4] = true;
+    this.lightArray[4][4] = true;
+  }
+
+  public get areAllLightsOn(): boolean {
+    for (var x = 0; x <= this.maxX; x++) {
+      for (var y = 0; y <= this.maxY; y++) {
+        if (!this.lightArray[x][y]) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  public toggle(x: number, y: number) {
+    const canDecreaseX = x > 0;
+    const canDecreaseY = y > 0;
+    const canIncreaseX = x < this.maxX;
+    const canIncreaseY = y < this.maxY;
+
+    this.lightArray[x][y] = !this.lightArray[x][y];
+
+    if (canDecreaseX) {
+      this.lightArray[x-1][y] = !this.lightArray[x-1][y];
+    }
+
+    if (canIncreaseX) {
+      this.lightArray[x+1][y] = !this.lightArray[x+1][y];
+    }
+
+    if (canDecreaseY) {
+      this.lightArray[x][y-1] = !this.lightArray[x][y-1];
+    }
+
+    if (canIncreaseY) {
+      this.lightArray[x][y+1] = !this.lightArray[x][y+1];
+    }
   }
 }
